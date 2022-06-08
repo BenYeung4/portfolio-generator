20fa7629c78800280ba77f673952d846ea6f6b59
Ben <ben@Benjamins-MacBook-Pro.local>
2022-06-08 19:10:29 -0400

    files

---
+++ app.js
@@ -0,0 +1,23 @@
+const profileDataArgs = process.argv.slice(2);
+
+//function outside , cannot use while inside
+const printProfileData = profileDataArr => {
+  //This...
+ for (let i = 0; i < profileDataArr.length; i += 1){
+  console.log(profileDataArr[i]);
+   }
+  console.log('===========');
+
+  //is the same as this...
+  profileDataArr.forEach(profileItem => console.log(profileItem));
+ };
+ printProfileData(profileDataArgs);
+
+//notice the lack of parentheses around the 'profileDataArr' parameter?
+// const printProfileData = profileDataArr => {
+//   for(let i=0; i< profileDataArr.length; i++){
+//     console.log(profileDataArr[i]);
+//   }
+// };
+ 
+
